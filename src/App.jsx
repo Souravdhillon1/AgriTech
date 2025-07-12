23ab5d3baac5cb57975ac584310b77b23b1dec55
Sourav Dhillon <souravdhillon50@gmail.com>
2025-07-12 12:06:39 +0530

    Deploy via docs folder for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,47 @@
-import Login from './home/Login'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Routes,Route } from 'react-router-dom'
-import './App.css'
-import Register from './home/Register'
-import Home from "./home/Home"
-import Search from './results/Search'
-import ForgotPassword from './home/ForgotPassword'
-import ResetPassword from './home/ResetPassword'
-import Cart from './results/Cart'
-import Address from './home/Address'
-import Help from './home/Help'
-import You from './home/You'
-import Settings from './home/Settings'
-import More from './home/More'
-import SellForm from './home/SellForm'
-import Menu from './home/Menu'
-import Payment from './home/Payment'
-function App() {
- 
+// src/App.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
 
+import Login from "./home/Login";
+import Register from "./home/Register";
+import Home from "./home/Home";
+import Search from "./results/Search";
+import ForgotPassword from "./home/ForgotPassword";
+import ResetPassword from "./home/ResetPassword";
+import Cart from "./results/Cart";
+import Address from "./home/Address";
+import Help from "./home/Help";
+import You from "./home/You";
+import Settings from "./home/Settings";
+import More from "./home/More";
+import SellForm from "./home/SellForm";
+import Menu from "./home/Menu";
+import Payment from "./home/Payment";
+
+function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/AgriTech">
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/search" element={<Search/>} />
+        <Route path="/search" element={<Search />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/address" element={<Address/>} />
-        <Route path="/help" element={<Help/>} />
-        <Route path="/you" element={<You/>} />
-        <Route path="/settings" element={<Settings/>} />
-        <Route path="/more" element={<More/>} />
-        <Route path="/sell" element={<SellForm/>} />
-        <Route path="/menu" element={<Menu/>} />
-        <Route path="/payment" element={<Payment/>} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/address" element={<Address />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/you" element={<You />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/more" element={<More />} />
+        <Route path="/sell" element={<SellForm />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
